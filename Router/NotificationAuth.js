d141e492291618f2af8902d6ec85f12877e851e6
Harshamasina <18uec021@lnmiit.ac.in>
2023-03-17 13:48:09 +0530

    changed

--- Router/NotificationAuth.js
+++ Router/NotificationAuth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const moment = require('moment/moment');
 const NotificationSchema = require('../Models/NotificationModel');
+const MISPatentsSchema = require('../Models/PatentModel');
 require('dotenv').config();
 
 router.get('/notifications', (req , res) => {
@@ -134,37 +135,41 @@ router.delete('/api/deletenotification/:id', async (req, res) => {
     }
 });
 
-router.get('/api/getnotifications', async (req, res) => {
+router.get('/api/pctnotifications', async (req, res) => {
     try{
-        const filteredData = await Data.find({
-            $or: [
-              { 'npe.npe_oa.npe_oa_date': { $lt: moment().add(60, 'days').toDate() } },
-              { 'npe.npe_af.npe_af_date': { $lt: moment().add(30, 'days').toDate() } },
-              { 'npe.npe_if': { $lt: moment().add(60, 'days').toDate() } },
-              { 'npe.npe_rfe': { $lt: moment().add(60, 'days').toDate() } },
-              { 'ref-no': { $lt: moment().add(60, 'days').toDate() } },
-              { 'pct_isr': { $lt: moment().add(60, 'days').toDate() } },
-              { 'pct_18_md': { $lt: moment().add(60, 'days').toDate() } },
-              { 'pct_22': { $lt: moment().add(60, 'days').toDate() } },
-              { 'pct_30': { $lt: moment().add(60, 'days').toDate() } }
-            ]
-        });
-        const mappedData = filteredData.map(data => {
-            const mappedDates = [];
-            if (data.npe.npe_oa && data.npe.npe_oa.npe_oa_date < moment().add(60, 'days').toDate()) {
-                mappedDates.push({
-                  fieldName: 'npe_oa_date',
-                  label: 'OA Date',
-                  dateValue: moment(data.npe.npe_oa.npe_oa_date).format('YYYY-MM-DD'),
-                  diffInDays: moment(data.npe.npe_oa.npe_oa_date).diff(moment(), 'days')
-                });
+        const fields = [
+            { name: 'pct_isr', label: 'PCT International Search Report' },
+            { name: 'pct_18', label: 'PCT 18 Month' },
+            { name: 'pct_22_md', label: 'PCT Publication' },
+            { name: 'pct_30_31', label: 'PCT 30/31 Month' },
+        ];
+        const currentDate = new Date();
+        let results = [];
+        const documents = await MISPatentsSchema.find();
+        for (const document of documents) {
+            for (const field of fields) {
+                const fieldValue = document[field.name];
+                if (fieldValue) {
+                  const fieldValueDate = new Date(fieldValue);
+                  const diffTime = Math.abs(currentDate - fieldValueDate);
+                  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+                    if (diffDays <= 60) {
+                        results.push({
+                            ref_no: document.ref_no,
+                            fieldName: field.label,
+                            fieldValue: fieldValue,
+                            daysLeft: diffDays
+                        });
+                    }
+                }
             }
-        });
-        res.status(200).json(sortedData);
+        }
+        results.sort((a, b) => a.daysLeft - b.daysLeft);
+        res.status(201).json(results);
     } catch(err){
         res.status(500).json({
             error: err,
-            message: "No notifications Found"
+            message: "Failed To get PCT Notifications"
         });
     }
 });
