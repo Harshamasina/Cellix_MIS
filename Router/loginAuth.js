1bb00090457ff6366ffa4428181e62616e2dc256
Harshamasina <18uec021@lnmiit.ac.in>
2023-04-22 15:46:11 +0530

    changed

--- Router/loginAuth.js
+++ Router/loginAuth.js
@@ -73,7 +73,7 @@ router.get('/api/getnumbers', async (req, res) => {
 
 router.get('/api/getemployees', async (req, res) => {
     try{
-        const employees = await EmployeeSchema.find({});
+        const employees = await EmployeeSchema.find({}).sort({ createdAt: -1 });
         res.status(201).json(employees);
     } catch (err) {
         res.status(500).json({
